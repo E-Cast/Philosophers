837d3d5b8e5fb9f1372074a14cbbf7922854b4ac
Emile Castonguay <emilcastonguay@gmail.com>
2024-05-04 19:33:03 -0400

    getting started

---
+++ philo/sources/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 19:21:48 by ecastong          #+#    #+#             */
+/*   Updated: 2024/05/04 19:32:04 by ecastong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+//set parameters
+	//set default parameters
+		//number of philosophers
+		//time to die
+		//time to eat
+		//time to sleep
+		//optional, number of times each must eat before stopping
+	//take input and process it
+	//set final parameters
+		//overwrite default parameters with user input as needed
+		//number of forks (number of philos + 1)
+		//...
+//initialize
+	//...
+//run
+	//...
+//terminate
+	//...
+
+int	main(int argc, char **argv)
+{
+	
+}
