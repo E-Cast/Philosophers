a518ba00c5c78aa9e4c18134c2bbc9a6a06bfc91
E-Cast <emilcastonguay@gmail.com>
2024-05-08 09:32:03 -0400

    modified structs

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 19:21:44 by ecastong          #+#    #+#             */
-/*   Updated: 2024/05/08 09:09:33 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/05/08 09:31:46 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,63 +55,75 @@ typedef struct s_parameters
 	int	times_eaten;
 }	t_params;
 
-typedef enum e_states
+typedef enum e_state
 {
 	EATING,
 	SLEEPING,
-	THINKING,
-	DEAD
-}	t_states;
+	THINKING
+}	t_state;
+
+typedef struct timeval	t_time;
 
 typedef struct s_philosopher
 {
-	unsigned int	id;
-	pthread_t		*thread;
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*state;
-	unsigned int	times_eaten;
-	unsigned int	time_last_eaten;
-
 	t_params		params;
+
+	int				id;
+	t_state			state;
+	t_time			time_last_eaten;
+	int				times_eaten;
+
+	pthread_mutex_t	*fork_l;
+	pthread_mutex_t	*fork_r;
+	pthread_mutex_t	*info_lock;
+	bool			*can_eat;
+	bool			*dead;
 }	t_philo;
 
 typedef struct s_table
 {
 	t_params		params;
-	bool			start_routine;
-	pthread_t		*threads;
-	t_philo			*philos;
+	int				*times_eaten;
+
 	pthread_mutex_t	*forks;
+	t_philo			*philos;
+	pthread_mutex_t	*info_lock;
+	bool			*can_eat;
+	bool			*dead;
+	pthread_t		*threads;
 }	t_table;
 
-// typedef struct s_table
+// typedef struct s_philosopher
 // {
-// 	unsigned int	philo_count;
-// 	unsigned int	fork_count;
-// 	unsigned int	time_to_die;
-// 	unsigned int	time_to_eat;
-// 	unsigned int	time_to_sleep;
-// 	int				times_eaten;
-// 	t_philo			*philos;
-// }	t_table;
+// 	unsigned int	id;
+// 	pthread_t		*thread;
+// 	pthread_mutex_t	*r_fork;
+// 	pthread_mutex_t	*l_fork;
+// 	pthread_mutex_t	*state;
+// 	unsigned int	times_eaten;
+// 	unsigned int	time_last_eaten;
+
+// 	t_params		params;
+// }	t_philo;
 
 /*params.c*/
 
+int		check_arg_count(int argc);
+int		ft_atoi(const char *str);
+int		check_params(t_params *p);
 int		set_params(t_params *params, int argc, char **argv);
 
-/*routine.c*/
+// /*routine.c*/
 
-void	*routine(void *param);
+// void	*routine(void *param);
 
-/*table.c*/
+// /*table.c*/
 
-void	init_philos(t_params params, t_table *table);
-int		init_table(t_table *table, t_params params);
+// void	init_philos(t_params params, t_table *table);
+// int		init_table(t_table *table, t_params params);
 
-/*utils.c*/
+// /*utils.c*/
 
-void	*ph_calloc(size_t count, size_t size);
-int		ph_atoui(const char *str);
+// void	*ph_calloc(size_t count, size_t size);
 
 #endif
